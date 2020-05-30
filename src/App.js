b6ad5a958b13399ed0fb316324a0a88c2218f7ce
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-05-30 11:08:32 +0100

    1.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BabyNamesData from "./babyNamesData.json"
+import ShowNames from './ShowNames.js';
 
 function App() {
   return (
@@ -8,19 +10,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Baby React Names
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className="App">
+        <ShowNames BabyNames={BabyNamesData}/>      
+      </div>
     </div>
   );
 }
-
 export default App;
