b4db77ee94c44a8c3289411d30321a27dd817617
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-06-19 12:08:59 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -11,29 +11,31 @@ const  App = () => {
   const [name, setName] = useState('');
   const [sex ,setSex] = useState("all");
   const [babyNames, setBabyNames] = useState ([]);
-  const [updateData , setUpdateData] = useState([]);
   const [favoriteNames, setFavoriteNames] = useState([]);
 
   const Change = e => {
     setName((e.target.value).toLowerCase())
   }
+  let finalBabyNames = data.sort((a, b) => a.name.localeCompare(b.name))
 
-  useEffect( () => {
-    setUpdateData(data.filter( item => item.name.toLowerCase().includes(name)))
-  }, [name])
-  
-  const finalBabyNames = (updateData.filter( item => item.name.toLowerCase().includes(name) && item.sex !== sex));
-  
+  if(sex === "f" || sex === "m"){ finalBabyNames = data.filter(baby => baby.sex === sex)}
+  if(name){finalBabyNames = (data.filter( item => item.name.toLowerCase().includes(name.toLowerCase())))};
+  if(name && (sex === "f" || sex === "m")){
+    finalBabyNames = data.filter(baby => baby.sex === sex).filter(baby => baby.name.toLowerCase().includes(name.toLowerCase()))
+  }
+
+  if(favoriteNames){finalBabyNames = finalBabyNames.filter(  name => favoriteNames.indexOf( name ) < 0 )}
+
   const favorite = (value) =>{
     setFavoriteNames([...favoriteNames, value]);
     const addNames = data.filter(name => name.id !== value.id);
-    setUpdateData(addNames)
+    setBabyNames(addNames)
   }
   const removeFavorites = value => {
     const updatedFavoriteNames = favoriteNames.filter(name => name.id !== value.id);
     setFavoriteNames(updatedFavoriteNames);
-    const updatedNames = [...updateData, value];
-    setUpdateData(updatedNames)
+    const updatedNames = [...babyNames, value];
+    setBabyNames(updatedNames)
     }
     
   const filterBySex = (selectSex) => {
@@ -47,8 +49,8 @@ const  App = () => {
         <div className="nav-bar">
           <input className="search" placeholder="Search names" onChange={Change}></input>
           <div  className='icons'>
-            <input type="radio" onClick={()=> filterBySex("f")} name="gender" value="male"></input><label>Boy</label>
-            <input type="radio" onClick={()=> filterBySex("m")} name="gender" value="female"></input><label>Girl</label>
+            <input type="radio" onClick={()=> filterBySex("m")} name="gender" value="male"></input><label>Boy</label>
+            <input type="radio" onClick={()=> filterBySex("f")} name="gender" value="female"></input><label>Girl</label>
             <input type="radio" onClick={()=> filterBySex("all")} name="gender" value="all"></input><label>All</label>
           </div>
           <div>
